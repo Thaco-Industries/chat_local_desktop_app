7923343685887734efd05215b360457b70f4e8e3
minhnc912 <46645672+minhnc912@users.noreply.github.com>
2024-12-20 13:19:04 +0700

    remove devtool í product

--- public/electron.js
+++ public/electron.js
@@ -64,9 +64,9 @@ function createWindow() {
   });
 
   // Automatically open Chrome's DevTools in development mode.
-  // if (!app.isPackaged) {
-  mainWindow.webContents.openDevTools();
-  // }
+  if (!app.isPackaged) {
+    mainWindow.webContents.openDevTools();
+  }
 }
 
 function setupLocalFilesNormalizerProxy() {
